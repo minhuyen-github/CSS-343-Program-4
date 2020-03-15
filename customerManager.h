04d740121a4a7ba37de651172db136eb0b4a0c3c
minhuyen-github <57023265+minhuyen-github@users.noreply.github.com>
2020-03-14 17:58:49 -0700

    Update customerManager.h

--- customerManager.h
+++ customerManager.h
@@ -1,10 +1,3 @@
-//
-//  store.hpp
-//  Program 4
-//
-//  Created by Uyen Minh Hoang on 3/9/20.
-//  Copyright © 2020 Uyen Minh Hoang. All rights reserved.
-//
 
 #ifndef customerManager_h
 #define customerManager_h
@@ -16,15 +9,19 @@
 #include <string>
 using namespace std;
 
+//-------------------------- class CustomerManager ---------------------------
+// Purpose: This class put each customer with their info into the HashTable.
+//          It can also retrieve a specified customer using their unique IDs.
+//----------------------------------------------------------------------------
 class CustomerManager
 {
 public:
     CustomerManager();                   // constructor
     ~CustomerManager();                  // destructor
-    void processCustomers(ifstream& customerFile);   // process file with customers
-    Customer* getCustomer(int);                          // returns a customer
+    void processCustomers(ifstream& customerFile);       // process file which contains customers' info
+    Customer* getCustomer(int);                          // returns a customer given a unique ID
     
 private:
-    HashTable customers;            //stores all customers
+    HashTable customers;            //stores all customers' info
 };
 #endif /* customerManager_h*/
