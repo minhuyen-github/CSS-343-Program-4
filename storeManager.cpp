cc72da84f548fddec0a4b01c7363469a3cf5c1b2
minhuyen-github <57023265+minhuyen-github@users.noreply.github.com>
2020-03-14 18:03:28 -0700

    Update storeManager.cpp

--- storeManager.cpp
+++ storeManager.cpp
@@ -1,29 +1,11 @@
-//
-//  storeManager.cpp
-//  Program 4
-//
-//  Created by Uyen Minh Hoang on 3/13/20.
-//  Copyright © 2020 Uyen Minh Hoang. All rights reserved.
-//
-
 #include "storeManager.h"
 #include <string>
 #include <iostream>
 #include <fstream>
 
-//--------------------------  class StoreManager ----------------------------
-// Purpose:
-//   -- Store Manager class: high level manager for other manager
-//      classes such as: CustomerManager, InventoryManager, ComandLoaderManager.
-//
-// Implementation and assumptions:
-//   -- Implement as a manager class
-//   -- Implement with following futures:
-//      buildStore, readCommands
-//---------------------------------------------------------------------------
-
 //----------------------------- buildStore ----------------------------------
-// --Processes inventory file(movieFile) and customer file(customerFile)
+// Description: This function processes movie file and customer file by using
+//              methods from class InventoryManager and CustomerManager
 //---------------------------------------------------------------------------
 void StoreManager::buildStore(ifstream& movie, ifstream& customer)
 {
@@ -32,7 +14,8 @@ void StoreManager::buildStore(ifstream& movie, ifstream& customer)
 }
 
 //------------------------------ readCommands -------------------------------
-// --Processes transaction commmands file (commands)
+// Description: This function processes transaction commmands file by using
+//              methods from class TransactionManager.
 //---------------------------------------------------------------------------
 void StoreManager::readCommands(ifstream& commands)
 {
