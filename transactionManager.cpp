4a0d45a732e997d8de2dbad238f4d0120939af24
minhuyen-github <57023265+minhuyen-github@users.noreply.github.com>
2020-03-14 18:04:33 -0700

    Update transactionManager.cpp

--- transactionManager.cpp
+++ transactionManager.cpp
@@ -1,12 +1,7 @@
 #include "transactionManager.h"
 
-//------------------------- class TransactionManager ------------------------
-// Purpose:
-//     -- Transaction Manager Processes the transaction file
-//---------------------------------------------------------------------------
-
 //----------------------------- Constructor ---------------------------------
-//
+// Descriptoion: This is the default constructor
 //---------------------------------------------------------------------------
 TransactionManager::TransactionManager()
 {
@@ -14,21 +9,17 @@ TransactionManager::TransactionManager()
 }
 
 //----------------------------- Destructor ----------------------------------
-//
+// Description: This is the destructor
 //---------------------------------------------------------------------------
 TransactionManager::~TransactionManager()
 {
     //empty on purpose
 }
 
-
 //--------------------------- readCommands -----------------------------------
-// --Function: read commands
-// --Param:
-//      --commandFile is a file with commands with 1 command per line
-// --Post-condition:
-//      --transaction commands are turned into a  string and then
-//        sent to be processed by transaction factory.
+// Description: This function reads command lines from command file
+// Post-condition: Transaction commands are turned into a string and then
+//                 sent to be processed by class TransactionFactory.
 //----------------------------------------------------------------------------
 void  TransactionManager::readCommands(ifstream& command, CustomerManager& customers, InventoryManager& inventory)
 {
@@ -40,16 +31,16 @@ void  TransactionManager::readCommands(ifstream& command, CustomerManager& custo
         getline(command, commandLine);    // get single command
         commandLine.erase(remove(commandLine.begin(), commandLine.end(), '\r'), commandLine.end()); //remove all /r
         
-        TransactionFactory doTransaction(commandLine, customers, inventory); // send it to transaction factory
+        TransactionFactory doTransaction(commandLine, customers, inventory); // send it to TransactionFactory
         string addToErrors = doTransaction.getErrors();
         
-        if (addToErrors != "" && addToErrors[0] == 'E')
+        if (addToErrors != "" && addToErrors[0] == 'E') //if found an error
         {
-            error.push_back(doTransaction.getErrors());
+            error.push_back(doTransaction.getErrors()); //push it to the vector
         }
     }
     
-    cout << "------------------ Transaction Errors --------------------" << "\n";
+    cout << "------------------ Transaction Errors --------------------" << "\n"; //display transaction errors
     
     for (int i = 0; i < error.size(); i++)
     {
