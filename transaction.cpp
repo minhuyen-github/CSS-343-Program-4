3958816c65ed441abd30d13a4e168cf5e4250c65
mxng113 <60676926+mxng113@users.noreply.github.com>
2020-03-14 17:18:54 -0700

    Update transaction.cpp

--- transaction.cpp
+++ transaction.cpp
@@ -1,23 +1,59 @@
-//
-//  transaction.cpp
-//  Program 4
-//
-//  Created by Uyen Minh Hoang on 3/5/20.
-//  Copyright © 2020 Uyen Minh Hoang. All rights reserved.
-//
+// ------------------------------------------------ transaction.cpp -------------------------------------------------------
 
+// Name: Uyen Hoang, Max Nguyen
+// CSS 343 Section C
+
+// Creation Date: 3/9/2020
+
+// Date of Last Modification: 3/14/2020
+
+// --------------------------------------------------------------------------------------------------------------------
+
+// This program is the implementation file (.cpp) that implements the parent class for all transactions
+
+// -------------------------------------------------------------------------------------------------------------------- 
 #include "transaction.h"
 
+// ----------------------------------- default constructor ---------------------------------------------- 
+
+// Description
+
+// default constructor: initialize an empty string for error
+
+// preconditions: none
+// postconditions: an empty string for error
+
+// ----------------------------------------------------------------------------------------------------- 
 Transaction::Transaction()
 {
     newError = "";
 }
 
+// --------------------------------------- destructor -------------------------------------------------- 
+
+// Description
+
+// destructor: virtual
+
+// preconditions: none
+// postconditions: none
+
+// ----------------------------------------------------------------------------------------------------- 
 Transaction::~Transaction()
 {
     //empty on purpose
 }
 
+// --------------------------------------- getError -------------------------------------------------- 
+
+// Description
+
+// getError: Return the new error
+
+// preconditions: none
+// postconditions: the error found
+
+// ----------------------------------------------------------------------------------------------------- 
 string Transaction::getError() const
 {
     return newError;
